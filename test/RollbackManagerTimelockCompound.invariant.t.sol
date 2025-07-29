17954da830104f9c613695471bc4d9f63e0875b8
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-07-30 00:08:55 +0530

    chore: address invariant PR feedback (#59)
    * chore: rename targets -> rollbackProposalTargets
    
    * chore: DRY invariant test handler
    
    * chore: DRY fuzzer config
    
    * chore: add fuzz param to DRY unit tests
    
    * chore: add fuzz param for testFuzz_ReturnsFalseForExpiredRollback
    
    * chore: add fuzz param for testFuzz_ReturnsFalseForQueuedRollbackBeforeDelay
    
    * chore: rename selectors -> proposalCalldataSelectors
    
    * DRY unit suite (#60)
    
    Co-authored-by: Aditya Anand M C <aditya.anandmc@gmail.com>
    
    ---------
    
    Co-authored-by: Ed Mazurek <Edward.R.Mazurek@gmail.com>

--- test/RollbackManagerTimelockCompound.invariant.t.sol
+++ test/RollbackManagerTimelockCompound.invariant.t.sol
@@ -11,67 +11,32 @@ import {MockCompoundTimelock} from "test/mocks/MockCompoundTimelock.sol";
 import {FakeProtocolContract} from "test/fakes/FakeProtocolContract.sol";
 import {RollbackManagerTimelockCompoundHandler} from "test/handlers/RollbackManagerTimelockCompound.handler.sol";
 import {RollbackManagerInvariantTestBase} from "test/helpers/RollbackManagerInvariantTestBase.sol";
-import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
-import {RollbackProposal} from "test/helpers/RollbackSet.sol";
 import {RollbackManagerHandlerBase} from "test/helpers/RollbackManagerHandlerBase.sol";
 
 contract RollbackManagerTimelockCompoundInvariantTest is RollbackManagerInvariantTestBase {
   RollbackManagerTimelockCompound public rollbackManager;
   MockCompoundTimelock public timelockTarget;
-  RollbackManagerTimelockCompoundHandler public handler;
+  RollbackManagerHandlerBase public handler;
 
-  function setUp() public override {
+  function _setupHandler() internal override {
     timelockTarget = new MockCompoundTimelock();
     timelockTarget.setDelay(delay);
 
-    targets = new FakeProtocolContract[](3);
-    targets[0] = new FakeProtocolContract(address(timelockTarget));
-    targets[1] = new FakeProtocolContract(address(timelockTarget));
-    targets[2] = new FakeProtocolContract(address(timelockTarget));
+    rollbackProposalTargets = new FakeProtocolContract[](3);
+    rollbackProposalTargets[0] = new FakeProtocolContract(address(timelockTarget));
+    rollbackProposalTargets[1] = new FakeProtocolContract(address(timelockTarget));
+    rollbackProposalTargets[2] = new FakeProtocolContract(address(timelockTarget));
 
     rollbackManager = new RollbackManagerTimelockCompound(address(timelockTarget), admin, guardian, delay, delay);
 
-    handler = new RollbackManagerTimelockCompoundHandler(rollbackManager, admin, guardian, targets);
-
-    // target the handler for invariant testing
-    targetContract(address(handler));
-
-    // Exclude handler iteration functions from fuzzing
-    bytes4[] memory excludeSelectors = new bytes4[](6);
-    excludeSelectors[0] = RollbackManagerHandlerBase.forEachRollbackQueuedButNotExecutable.selector;
-    excludeSelectors[1] = RollbackManagerHandlerBase.forEachRollbackByState.selector;
-    excludeSelectors[2] = RollbackManagerHandlerBase.forEachRollback.selector;
-    excludeSelectors[3] = RollbackManagerHandlerBase.getRollbackSetCount.selector;
-    excludeSelectors[4] = RollbackManagerHandlerBase.getRollbackProposal.selector;
-    excludeSelectors[5] = RollbackManagerHandlerBase.callSummary.selector;
-
-    excludeSelector(FuzzSelector(address(handler), excludeSelectors));
+    handler = new RollbackManagerTimelockCompoundHandler(rollbackManager, admin, guardian, rollbackProposalTargets);
   }
 
   function _getRollbackManager() internal view override returns (RollbackManager) {
     return rollbackManager;
   }
 
-  function _getHandler() internal view override returns (address) {
-    return address(handler);
-  }
-
-  function _forEachRollbackByState(IGovernor.ProposalState _state, function(RollbackProposal memory) external _func)
-    internal
-    override
-  {
-    handler.forEachRollbackByState(_state, _func);
-  }
-
-  function _forEachRollbackQueuedButNotExecutable(function(RollbackProposal memory) external _func) internal override {
-    handler.forEachRollbackQueuedButNotExecutable(_func);
-  }
-
-  function _callSummary() internal override {
-    handler.callSummary();
-  }
-
-  function invariant_callSummary() public override {
-    handler.callSummary();
+  function _getHandler() internal view override returns (RollbackManagerHandlerBase) {
+    return handler;
   }
 }
--- test/RollbackManagerTimelockControl.invariant.t.sol
+++ test/RollbackManagerTimelockControl.invariant.t.sol
@@ -11,67 +11,32 @@ import {MockTimelockTargetControl} from "test/mocks/MockTimelockTargetControl.so
 import {FakeProtocolContract} from "test/fakes/FakeProtocolContract.sol";
 import {RollbackManagerTimelockControlHandler} from "test/handlers/RollbackManagerTimelockControl.handler.sol";
 import {RollbackManagerInvariantTestBase} from "test/helpers/RollbackManagerInvariantTestBase.sol";
-import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
-import {RollbackProposal} from "test/helpers/RollbackSet.sol";
 import {RollbackManagerHandlerBase} from "test/helpers/RollbackManagerHandlerBase.sol";
 
 contract RollbackManagerTimelockControlInvariantTest is RollbackManagerInvariantTestBase {
   RollbackManagerTimelockControl public rollbackManager;
   MockTimelockTargetControl public timelockTarget;
-  RollbackManagerTimelockControlHandler public handler;
+  RollbackManagerHandlerBase public handler;
 
-  function setUp() public override {
+  function _setupHandler() internal override {
     timelockTarget = new MockTimelockTargetControl();
     timelockTarget.setMinDelay(delay);
 
-    targets = new FakeProtocolContract[](3);
-    targets[0] = new FakeProtocolContract(address(timelockTarget));
-    targets[1] = new FakeProtocolContract(address(timelockTarget));
-    targets[2] = new FakeProtocolContract(address(timelockTarget));
+    rollbackProposalTargets = new FakeProtocolContract[](3);
+    rollbackProposalTargets[0] = new FakeProtocolContract(address(timelockTarget));
+    rollbackProposalTargets[1] = new FakeProtocolContract(address(timelockTarget));
+    rollbackProposalTargets[2] = new FakeProtocolContract(address(timelockTarget));
 
     rollbackManager = new RollbackManagerTimelockControl(address(timelockTarget), admin, guardian, delay, delay);
 
-    handler = new RollbackManagerTimelockControlHandler(rollbackManager, admin, guardian, targets);
-
-    // target the handler for invariant testing
-    targetContract(address(handler));
-
-    // Exclude handler iteration functions from fuzzing
-    bytes4[] memory excludeSelectors = new bytes4[](6);
-    excludeSelectors[0] = RollbackManagerHandlerBase.forEachRollbackQueuedButNotExecutable.selector;
-    excludeSelectors[1] = RollbackManagerHandlerBase.forEachRollbackByState.selector;
-    excludeSelectors[2] = RollbackManagerHandlerBase.forEachRollback.selector;
-    excludeSelectors[3] = RollbackManagerHandlerBase.getRollbackSetCount.selector;
-    excludeSelectors[4] = RollbackManagerHandlerBase.getRollbackProposal.selector;
-    excludeSelectors[5] = RollbackManagerHandlerBase.callSummary.selector;
-
-    excludeSelector(FuzzSelector(address(handler), excludeSelectors));
+    handler = new RollbackManagerTimelockControlHandler(rollbackManager, admin, guardian, rollbackProposalTargets);
   }
 
   function _getRollbackManager() internal view override returns (RollbackManager) {
     return rollbackManager;
   }
 
-  function _getHandler() internal view override returns (address) {
-    return address(handler);
-  }
-
-  function _forEachRollbackByState(IGovernor.ProposalState _state, function(RollbackProposal memory) external _func)
-    internal
-    override
-  {
-    handler.forEachRollbackByState(_state, _func);
-  }
-
-  function _forEachRollbackQueuedButNotExecutable(function(RollbackProposal memory) external _func) internal override {
-    handler.forEachRollbackQueuedButNotExecutable(_func);
-  }
-
-  function _callSummary() internal override {
-    handler.callSummary();
-  }
-
-  function invariant_callSummary() public override {
-    handler.callSummary();
+  function _getHandler() internal view override returns (RollbackManagerHandlerBase) {
+    return handler;
   }
 }
